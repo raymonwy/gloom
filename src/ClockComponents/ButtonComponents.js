7bfa6221609a157535c7b49340663ca56edea9ce
raymonwy <raymondyang38@gmail.com>
2021-03-11 21:03:13 -0800

    adding icons

--- src/ClockComponents/ButtonComponents.js
+++ src/ClockComponents/ButtonComponents.js
@@ -6,31 +6,28 @@ function ButtonComponents(props) {
        <div>
            {(props.status === 0)?
                 <div >   
-                <button className="startButton" onClick ={props.start}  > start </button>
+                <button className="playButton" onClick ={props.start}  > start </button>
                 </div> : ""
 
          }
             {(props.status === 1)?
                 <div>
-                    <button className="stopButton" onClick ={props.stop} > stop </button>
-                    
+                    <button className="playButton" onClick ={props.stop} > stop </button>
                 </div> : ""
             }
 
             {(props.status === 2)?
                 <div>
-                    <button className="resumeButton" onClick ={props.start} > resume </button>
-                    
+                    <button className="playButton" onClick ={props.start} > resume </button>
                 </div> : ""
             }
             
             {(props.status !== 0)?
                 <div >
-                    
-                    <button className="resetButton" onClick ={props.reset} > reset </button>
+                    <button type="submit" className="resetButton" onClick ={props.reset} > reset </button>
                 </div> : ""
             }       
-
+            
        </div>
       );
 }
