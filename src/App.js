30d4c189da1de665a3f8b8b8580e64b7bd6f325d
raymonwy <raymondyang38@gmail.com>
2021-03-12 00:18:50 -0800

    blinking

--- src/App.js
+++ src/App.js
@@ -9,18 +9,42 @@ function App() {
   const [interv,setInterv] = useState();
   const [status, setStatus] = useState(0);
   var updatedM = time.m, updatedS = time.s;
-  
+  var bucket = 0;
+
   function start(){
     run();
+    
     setStatus(1);
     setInterv(setInterval(run,1000));
   }
+
+  function blink(){
+    var border = document.getElementById('border');
+    console.log(border.style.animationIterationCount );
+    border.style.animation = 'none';
+    setTimeout(function() {border.style.animation = '';},10);
+  }
+
+  function test(){
+    var border = document.getElementById('border');
+    border.style.animation = 'none';
+  }
+
   function run() {
+    console.log(bucket);
     if (updatedS === 0){
       updatedM--;
       updatedS = 60;
     }
+    if (bucket === 20){
+      bucket = 0;
+      test();
+    }
+    if (bucket === 10){
+      blink();
+    }
     updatedS--;
+    bucket++;
     return setTime({m:updatedM,s:updatedS});
   }
 
@@ -39,7 +63,7 @@ function App() {
     <section className="timer-container">
       <section className="timer">
        
-        <div className="digits">
+        <div id = "border" className="digits">
           <DisplayComponents time = {time}/>
           
         </div>
--- src/ClockComponents/ButtonComponents.js
+++ src/ClockComponents/ButtonComponents.js
@@ -1,30 +1,31 @@
 
 import React from 'react';
+import refresh from '../pics/reset.svg'
 
 function ButtonComponents(props) {
     return (
        <div>
            {(props.status === 0)?
                 <div >   
-                <button className="playButton" onClick ={props.start}  > start </button>
+                <button className="playButton" title="play" onClick ={props.start}  >  </button>
                 </div> : ""
 
          }
             {(props.status === 1)?
                 <div>
-                    <button className="playButton" onClick ={props.stop} > stop </button>
+                    <button className="playButton" title="stop" onClick ={props.stop} >  </button>
                 </div> : ""
             }
 
             {(props.status === 2)?
                 <div>
-                    <button className="playButton" onClick ={props.start} > resume </button>
+                    <button className="playButton" title="resume" onClick ={props.start} >  </button>
                 </div> : ""
             }
             
             {(props.status !== 0)?
                 <div >
-                    <button type="submit" className="resetButton" onClick ={props.reset} > reset </button>
+                    <img src={refresh}  className="resetButton" title="reset" alt="reset" onClick ={props.reset}></img>
                 </div> : ""
             }       
             
